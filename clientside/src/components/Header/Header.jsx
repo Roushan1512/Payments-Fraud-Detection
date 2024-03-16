f047e1cece9bba76a5f908037e67e259273b6196
Avirup Ray <avirupraytun@gmail.com>
2024-03-16 21:15:50 +0530

    few changes

--- clientside/src/components/Header/Header.jsx
+++ clientside/src/components/Header/Header.jsx
@@ -1,16 +1,55 @@
 import React from "react";
 import logo from "./Cypher_logo.png";
+import { Link, NavLink } from "react-router-dom";
 const Header = () => {
   return (
-    <div className="bg-[#000000] flex justify-between items-center  text-white mt-6 left-[20%] 2xl:h-[3.5rem] h-24 w-[60vw] rounded-3xl px-10 overflow-hidden  shadow-[#120411] absolute z-30">
+    <div className="bg-[#000000] flex justify-between items-center  text-white mt-6 left-[20%] h-[6vh] w-[60vw] rounded-3xl px-10 overflow-hidden  shadow-[#120411] absolute z-30">
       <div>
-        <img src={logo} alt="" className="w-[5.5rem]" />
+        <NavLink to="/">
+          <img src={logo} alt="" className="h-[8vh]" />
+        </NavLink>
       </div>
       <div className="">
         <ul className="flex justify-center items-center gap-5 ">
-          <li>HOME</li>
-          <li>DASHBOARD</li>
-          <li>ABOUT</li>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              ` ${
+                isActive
+                  ? "bg-[#6d006d] hover:bg-[#940194]"
+                  : "bg-transparent hover:bg-[#450a45]"
+              }
+              p-2 rounded-2xl transition ease-in-out`
+            }
+          >
+            <li>HOME</li>
+          </NavLink>
+          <NavLink
+            to="/dashboard"
+            className={({ isActive }) =>
+              ` ${
+                isActive
+                  ? "bg-[#6d006d] hover:bg-[#940194]"
+                  : "bg-transparent hover:bg-[#450a45]"
+              }
+              p-2 rounded-2xl transition ease-in-out`
+            }
+          >
+            <li>DASHBOARD</li>
+          </NavLink>
+          <NavLink
+            to="/aboutus"
+            className={({ isActive }) =>
+              ` ${
+                isActive
+                  ? "bg-[#6d006d] hover:bg-[#940194]"
+                  : "bg-transparent hover:bg-[#450a45]"
+              }
+              p-2 rounded-2xl transition ease-in-out `
+            }
+          >
+            <li>ABOUT</li>
+          </NavLink>
         </ul>
       </div>
       <div>LOGIN</div>
