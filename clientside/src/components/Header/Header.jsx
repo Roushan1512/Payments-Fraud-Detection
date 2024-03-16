8e2335fe6bd240b771853fd893b818e524899b03
Avirup Ray <avirupraytun@gmail.com>
2024-03-16 14:57:46 +0530

    header changed

--- clientside/src/components/Header/Header.jsx
+++ clientside/src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="bg-[#000000] flex justify-between items-center text-white m-10 mx-32 h-12 rounded-3xl px-10 overflow-hidden shadow-xl shadow-[#120411]">
+    <div className="bg-[#000000] flex justify-between items-center text-white mt-6 left-[20%] h-24 w-[60vw] rounded-3xl px-10 overflow-hidden  shadow-[#120411] absolute">
       <div>LOGO</div>
       <div className="">
         <ul className="flex justify-center items-center gap-5 ">
--- clientside/tailwind.config.js
+++ clientside/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Amita: ["Amita", "cursive"],
+        Angkor: ["Angkor", "monospace"],
+      },
+    },
   },
   plugins: [],
 };
