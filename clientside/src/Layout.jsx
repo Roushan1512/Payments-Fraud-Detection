70a8874fdc7ac2036fe04d4edecbf3ec976e41a3
Not-Sahil-Raja <sahilraja2002@gmail.com>
2024-03-16 19:18:55 +0530

    Dashboard added

--- clientside/src/Layout.jsx
+++ clientside/src/Layout.jsx
@@ -5,11 +5,11 @@ import Footer from "./components/Footer/Footer";
 
 function Layout() {
   return (
-    <>
+    <div className=" bg-green-500 overflow-x-hidden">
       <Header />
       <Outlet />
       <Footer />
-    </>
+    </div>
   );
 }
 
---
+++ clientside/src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+const Dashboard = () => {
+  return (
+    <div className=" w-[100vw] h-[100vh] bg-red-400 overflow-x-hidden"></div>
+  );
+};
+
+export default Dashboard;
