ebbfb5871c8997557b7781aa5b652793fe39cb9a
Roushan1512 <roxie4269@gmail.com>
2024-03-16 15:25:27 +0530

    login options enabled

--- clientside/src/main.jsx
+++ clientside/src/main.jsx
@@ -9,7 +9,7 @@ import {
 import "./index.css";
 import Homepage from "./components/Home/Homepage.jsx";
 import Layout from "./Layout.jsx";
-// import { Auth0Provider } from "@auth0/auth0-react";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const AppRouter = createBrowserRouter([
   {
@@ -26,14 +26,12 @@ const AppRouter = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <Auth0Provider
-      domain=
-      clientId=
-      authorizationParams={{
-        redirect_uri: window.location.origin,
-      }}
-    > */}
-    <RouterProvider router={AppRouter} />
-    {/* </Auth0Provider> */}
+    <Auth0Provider
+      domain="dev-s2qldime8th57mtv.jp.auth0.com"
+      clientId="84iyPmZ6Ig4J9YpotTDkhCAEpCkhyoDL"
+      authorizationParams={{ redirect_uri: window.location.origin }}
+    >
+      <RouterProvider router={AppRouter} />
+    </Auth0Provider>
   </React.StrictMode>
 );
