57657fc70c5119bda516011cdb9b541707e3f3b3
Not-Sahil-Raja <sahilraja2002@gmail.com>
2024-03-16 15:46:37 +0530

    NextUI Added

--- clientside/src/main.jsx
+++ clientside/src/main.jsx
@@ -10,6 +10,8 @@ import "./index.css";
 import Homepage from "./components/Home/Homepage.jsx";
 import Layout from "./Layout.jsx";
 import { Auth0Provider } from "@auth0/auth0-react";
+import Dashboard from "./components/Dashboard/Dashboard.jsx";
+import { NextUIProvider } from "@nextui-org/react";
 
 const AppRouter = createBrowserRouter([
   {
@@ -20,18 +22,24 @@ const AppRouter = createBrowserRouter([
         path: "/",
         element: <Homepage />,
       },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
     ],
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Auth0Provider
-      domain="dev-s2qldime8th57mtv.jp.auth0.com"
-      clientId="84iyPmZ6Ig4J9YpotTDkhCAEpCkhyoDL"
-      authorizationParams={{ redirect_uri: window.location.origin }}
-    >
-      <RouterProvider router={AppRouter} />
-    </Auth0Provider>
+    <NextUIProvider>
+      <Auth0Provider
+        domain="dev-s2qldime8th57mtv.jp.auth0.com"
+        clientId="84iyPmZ6Ig4J9YpotTDkhCAEpCkhyoDL"
+        authorizationParams={{ redirect_uri: window.location.origin }}
+      >
+        <RouterProvider router={AppRouter} />
+      </Auth0Provider>
+    </NextUIProvider>
   </React.StrictMode>
 );
